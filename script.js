08ba7d6a2ca0c71fdc135ab0a78eb4e975e6adeb
amitkumar <realamit678@gmail.com>
2022-03-28 13:39:43 +0530

    no commit

---
+++ script.js
@@ -0,0 +1,61 @@
+// for(var i=1;i<=10;i++){
+//     console.log(2+"*"+i+" ="+2*i )
+// }
+// var name='amit'
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// var name='amit'
+// for(var i=0;i<=10;i++){
+//     console.log('amit')
+// }
+// for(var i=1;i<=10;i++){
+//     console.log(2+"X"+i+"="+2*i)
+// }
+// var i=1;
+// while(i<10){
+//     console.log(i);
+//     i++
+// }
+// var num=12;
+// var lastdigit=2;
+// var sumofalldigit=0;
+
+// var lastdigit=num%10;
+// sumofalldigit=sumofalldigit+lastdigit; //2
+// num=(parseInt(num/10)); //1.2
+
+
+// lastdigit=num%10;
+// sumofalldigit=sumofalldigit+lastdigit;
+// num=(parseInt(num/10))
+
+// lastdigit=num%10;
+// sumofalldigit=sumofalldigit+lastdigit;
+// num=(parseInt(num/10))
+
+// lastdigit=num%10;
+// sumofalldigit=sumofalldigit+lastdigit;
+// num=(parseInt(num/10))
+
+// console.log(sumofalldigit)
+
+// var num=12;
+// var lastdigit=0;
+// var sumofalldigit=0;
+// while(num!=0){
+//     var lastdigit=num%10;
+//     sumofalldigit=sumofalldigit+lastdigit;
+//     num=(parseInt(num/10));
+    
+// }
+// console.log(sumofalldigit);
+// var i=1;
+// do{
+//     console.log(i);
+//     i++
+// }
+// while(i<10)
